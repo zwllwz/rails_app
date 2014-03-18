ff4e28c3418fe6096744b06758b0c9ac9b06b66b
zwllwz <zwllwz.z@gmail.com>
2014-03-17 20:07:31 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Itrade::Application.config.secret_key_base = '911d8ad544b1b62d6e2e7b791688ae550196adb46818519d1a8ed1b54827debc683bb268b93d5cb4eae9a6d58b7a93ddc9616d6420aa1a6a00bb10761f752c08'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Itrade::Application.config.session_store :cookie_store, key: '_itrade_session'
